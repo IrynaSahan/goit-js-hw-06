080321c4e4c2cc80079264f56129bb65623b7dd4
IrynaSahan <sahanirasah@gmail.com>
2023-01-13 00:38:07 +0200

    task-05

--- js/task-05.js
+++ js/task-05.js
@@ -6,10 +6,8 @@ const refs = {
 refs.input.addEventListener('input', onInputChange);
 
 function onInputChange(event) {
-    if (refs.span.textContent.length > 0,
-        refs.span.textContent = event.currentTarget.value);
-    else (refs.span.textContent.length === 0,
-        event.currentTarget = refs.span.textContent = 'Anonymous');
+       event.currentTarget.value.length > 0 ? refs.span.textContent = event.currentTarget.value
+        : refs.span.textContent = 'Anonymous';
 };
 
 
