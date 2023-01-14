542be0e8656fc5a6281e2a1f39b245c47edf9005
IrynaSahan <sahanirasah@gmail.com>
2023-01-15 00:41:52 +0200

    update task-8 (reset)

--- js/task-08.js
+++ js/task-08.js
@@ -7,16 +7,18 @@ function onFormSubmit(event) {
     // console.log('це сабміт форми');
     const formElem = event.currentTarget.elements;
 
-    if (formElem.email.value.trim() === '' || formElem.password.value === '') {
+    if (formElem.email.value.trim() === '' || formElem.password.value.trim() === '') {
         return alert('Всі поля повинні бути заповнені!');
     }
 
     const formData = new FormData(event.currentTarget);  
     let newObjectData = Object.fromEntries(formData);
-     console.log(newObjectData);
-    
-     form.reset();
-
+    console.log(newObjectData);
+    event.currentTarget.reset();
 }
+
+
+
+
 
 
